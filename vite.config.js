dd5b5dbd25c2c82f575f7783e9ace2d47f502b66
stanleytk <kim4111@purdue.edu>
2024-05-25 03:03:54 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/stanleytk.github.io/',
   plugins: [react()],
+  base: '/stanleytk.github.io/',
 });
